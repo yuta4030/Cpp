b1269d6af97d2bd25baa07d97b377adfb4869599
yuta4030 <hal4030@gmail.com>
2018-09-12 01:06:35 +0900

    init commit and gitignore

---
+++ prepare.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+if [ "$1" = "" ] ; then
+	echo "大会ナンバー3桁を入力"
+	exit 1
+fi
+
+ATCODER_URL=https://beta.atcoder.jp/contests/abc$1/tasks/abc$1_
+mkdir -m 777 $1
+
+cp ./sample.cpp ./$1/a.cpp
+cp ./sample.cpp ./$1/b.cpp
+cp ./sample.cpp ./$1/c.cpp
+cp ./sample.cpp ./$1/d.cpp
+
+cd ./$1
+sed -i '' -e s#atcoderURL#"$ATCODER_URL"a# a.cpp
+sed -i '' -e s#atoderURL#"$ATCODER_URL"b# b.cpp
+sed -i '' -e s#atcoderURL#"$ATCODER_URL"c# c.cpp
+sed -i '' -e s#atcoderURL#"$ATCODER_URL"d# d.cpp
+
+echo "g++ -std=c++11 a.cpp -o a.exe" > compile.sh
+echo "g++ -std=c++11 b.cpp -o b.exe" >> compile.sh
+echo "g++ -std=c++11 c.cpp -o c.exe" >> compile.sh
+echo "g++ -std=c++11 d.cpp -o d.exe" >> compile.sh
+
+bash ./compile.sh
